e7d5933695a96c89aa1f6d01b03847703f3202c9
HieuNM6 <hieunmce@gmail.com>
2016-10-15 21:59:12 +0700

    Implement touch command

--- touch.js
+++ touch.js
@@ -5,7 +5,17 @@ const fs = require('fs').promise
 
 
 async function touch() {
-  await fs.writeFile(process.argv[2], '').catch(err => console.log(err));
+  const fd = await fs.open(process.argv[2], 'a')
+    .catch((err) => {
+      console.log(err);
+    });
+  const stat = await fs.stat(process.argv[2])
+  await fs.futimes(fd, stat.atime, new Date())
+    .catch((err) => {
+      console.log(err);
+    });
+  fs.close(fd);
 }
+
 
 process.argv[2] ? touch() : console.log('Missing file name');
